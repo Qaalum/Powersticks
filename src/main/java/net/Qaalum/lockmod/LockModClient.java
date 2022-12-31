e7b5119d2e9ca42d3e68fd66c267b1ad9fe2d824
BuildTools <unconfigured@null.spigotmc.org>
2023-01-01 02:00:59 +0400

    Initial commit

---
+++ src/main/java/net/Qaalum/lockmod/LockModClient.java
@@ -0,0 +1,10 @@
+package net.Qaalum.lockmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LockModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
